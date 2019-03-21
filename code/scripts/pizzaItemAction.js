86fb09605137e3291f2c6df192f93a9ad8182144
MasterDjefer <kostia210@gmail.com>
2019-03-21 19:08:58 +0200

    fix pizzaItem bugs(select, pizzaCount alignment)

--- code/scripts/pizzaItemAction.js
+++ code/scripts/pizzaItemAction.js
@@ -5,19 +5,29 @@ document.getElementById("decCount").onclick = function() {
     let count = parseInt(countPizza.innerHTML);
 
     countPizza.innerHTML = count - 1 < 1 ? 1 : count - 1;
-
-    recountpizzaPrice();
+    recountPizzaPrice();
+    recountLeftMarginCountPizza();
 };
 
 document.getElementById("incCount").onclick = function() {
     let countPizza = document.getElementById("countPizza");
     let count = parseInt(countPizza.innerHTML);
 
-    countPizza.innerHTML = parseInt(countPizza.innerHTML) + 1;
-    recountpizzaPrice();
+    countPizza.innerHTML = count + 1 === 100 ? 99 : count + 1;
+    recountPizzaPrice();
+    recountLeftMarginCountPizza();
 };
 
-function recountpizzaPrice() {
-    document.getElementById("countPrice").innerHTML = parseInt(document.getElementById("countPizza").innerHTML) * pizzaPrice + " UAH";
-    
+function recountPizzaPrice() {
+    document.getElementById("countPrice").innerHTML = parseInt(document.getElementById("countPizza").innerHTML) * pizzaPrice + " UAH";    
+}
+
+function recountLeftMarginCountPizza() {
+    let countPizza = document.getElementById("countPizza");
+    let count = parseInt(countPizza.innerHTML);
+    if (count < 10) {
+        countPizza.style.left = "7px";
+    } else {
+        countPizza.style.left = "0px";
+    }
 }
