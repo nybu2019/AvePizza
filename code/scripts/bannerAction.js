bf4b75002ba3f525011257c69509ade8fc80df26
MasterDjefer <kostia210@gmail.com>
2019-03-21 08:09:06 +0200

    simplified some code in the banner

--- code/scripts/bannerAction.js
+++ code/scripts/bannerAction.js
@@ -1,34 +1,15 @@
 let indexBanner = 1;
 let bannerInterval = 4000;
 
-document.body.onload = function() {
-    setStartImagesPosition();
+window.onload = function() {
     let bannerHandler = setInterval(loopImages, bannerInterval);
-    
+
     document.getElementById("mainBanner").onmouseover = function() {
         clearInterval(bannerHandler);
     };
     document.getElementById("mainBanner").onmouseleave = function() {
         bannerHandler = setInterval(loopImages, bannerInterval);
     };
-}
-
-function setStartImagesPosition() {
-    let img1 = document.getElementById("img1");
-    let img2 = document.getElementById("img2");
-    let img3 = document.getElementById("img3"); 
-
-    img2.style.opacity = "0";
-
-    setTimeout(function() {
-        img1.style.right = "800px";
-        img3.style.right = "0px";
-        img2.style.right = "-800px";
-    }, 500);
-
-    setTimeout(function() {
-        img2.style.opacity = "1";
-    }, 1000);
 }
 
 function loopImages() {
@@ -133,7 +114,7 @@ function loopImagesReverse() {
     }
 }
 
-document.getElementById("leftBannerNavi").onclick = function() { 
+document.getElementById("leftBannerNavi").onclick = function() {
     indexBanner = indexBanner - 1 < 1 ? 3 : indexBanner - 1;
 
     loopImagesReverse();
