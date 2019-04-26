df1dd95cec3d0f25d48c05aa0fe97753f8397eca
MasterDjefer <kostia210@gmail.com>
2019-04-26 14:13:46 +0300

    delete files with banner AGAIN! and improve headerNav(need fix)

--- code/scripts/mainAction.js
+++ code/scripts/mainAction.js
@@ -3,6 +3,19 @@ document.getElementById("testButton").onclick = function() {
     increaseGoodsCount(1);
 };
 
+window.onwheel = function(event) {
+    const headerNav = document.getElementsByClassName("headerNav")[0];
+
+    if (event.deltaY > 0) {
+        headerNav.style.top = "0px";
+    } else {
+
+        headerNav.style.top = "-100px";
+    }
+
+    console.log(event);
+};
+
 function addGoodsPrice(price) {
     let basketPrice = document.getElementsByClassName("basketPrice")[0];
     let currentPrice = parseInt(basketPrice.innerHTML.slice(0, basketPrice.innerHTML.length - 1));
