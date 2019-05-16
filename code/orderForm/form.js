4dc0c0657163704e61f777b9717af19aba0db642
Khrystyna Korinetska <kornecka@rambler.ru>
2019-05-16 19:30:50 +0300

    Styling forms

---
+++ code/orderForm/form.js
@@ -0,0 +1,21 @@
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
+
+document.getElementById('datePicker').value = new Date().toDateInputValue();
+
+
+function openbox(showArea) {
+    display = document.getElementById('showArea').style.display;
+
+    if (display=='none') {
+       document.getElementById('showArea').style.display='block';
+
+    } else {
+       document.getElementById('showArea').style.display='none';
+    }
+
+}
+
