85e39971d204805294d154dea8118f4b13fc7295
Khrystyna Korinetska <kornecka@rambler.ru>
2019-06-11 00:14:21 +0300

    Added takeFrom input, worked on the form switch, +code refactoring

--- code/orderForm/form.js
+++ code/orderForm/form.js
@@ -1,32 +1,39 @@
-// Change date format
+// Change date format. Will be changed
 Date.prototype.toDateInputValue = (function() {
     var local = new Date(this);
     local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
     return local.toJSON().slice(0,10);
 });
 
-// Update date
+// Update date. Will be changed
 document.getElementById('datePicker').value = new Date().toDateInputValue();
-let displayArea = false;
 
-
-document.getElementsByClassName('addComment')[0].onclick = function() {
-    document.getElementById('showArea').style.display = displayArea ? "none" : "block";
-    displayArea = !displayArea;
-    // need .formwrap height += .showarea heigth
-};
 
 $(document).ready(function() { 
+	// need .formWrap height += .showArea heigth
+	$(".addComment").on("click", () => {
+	$("#showArea").toggle();
+});
 
+	// Need to change switch. I think about it
 	$(".take-yourself").on("click", () => {
 	$(".block-hide").hide();
+	$(".takeFrom").show();
+	
+	$(".deliveryInfo").css({
+		width: "140px",
+		paddingRight: "0"
+	});
+
 	$(".formWrap").css("height", "420px");
-})
+});
 
  	$(".courier-delivery").on("click", () => {
 	$(".block-hide").show();
+	$(".takeFrom").hide();
+	$(".deliveryInfo").css("width", "200px");
 	$(".formWrap").css("height", "650px");
 	
-	 })
+	 });
 
 });
