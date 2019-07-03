39757c59b7e8a4310be64e1875b370cd9ce5fa4c
MasterDjefer <kostia210@gmail.com>
2019-07-03 21:20:41 +0300

    add ability to check collections size(need fix)

---
+++ server/mongoDB/initDb/Makefile.sh
@@ -0,0 +1,1 @@
+nodejs initDb.js
--- server/mongoDB/initDb/initDb.js
+++ server/mongoDB/initDb/initDb.js
@@ -1,10 +1,41 @@
-let MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
+const fs = require("fs");
 
-let url = "mongodb://localhost:27017/";
+const url = "mongodb://localhost:27017/";
 
-const dbName = "pizzasDatabase2";
+const dbName = "pizzasDatabase1";
 const pizzasCollectionName = "pizzas";
 const orderCollectionName = "orders";
+
+/** 
+ * @param {string} fileName 
+ */
+function fillCollection(collection, fileName) {
+    let flag = true;
+    collection.countDocuments(function(err, res) {
+        if (err) throw err;
+
+        flag = !res;
+        console.log(flag);
+    });
+    console.log(flag);
+    
+    
+    if (flag) {     
+        const data = fs.readFileSync(fileName);  
+        const parsedData = JSON.parse(data);
+
+        parsedData.pizzas.forEach(function(pizzaObj) {
+            collection.insertOne(pizzaObj, function(err, res) {
+                if (err) throw err;            
+                console.log("object is inserted");
+            });
+        });
+    }
+    else {
+        console.log("already filled");
+    }
+}
 
 
 function initDatabase() {
@@ -18,22 +49,25 @@ function initDatabase() {
         const orderCollection = database.collection(orderCollectionName);
         console.log("Collection " + orderCollectionName + " is added.");
 
-        pizzasCollection.insertOne({title: "diablo", weight: 45}, function(err, res) {
-            if (err) throw err;
-            console.log("1 object is inserted");
-        });
-        pizzasCollection.insertOne({title: "palermo", weight: 20}, function(err, res) {
-            if (err) throw err;
-            console.log("1 object is inserted");
-        });
-        pizzasCollection.insertOne({title: "neapoly", weight: 36}, function(err, res) {
-            if (err) throw err;
-            console.log("1 object is inserted");
-        });
-        pizzasCollection.insertOne({title: "ukr", weight: 80}, function(err, res) {
-            if (err) throw err;
-            console.log("1 object is inserted");
-        });
+
+        fillCollection(pizzasCollection, pizzasCollectionName + ".json");
+
+        // pizzasCollection.insertOne({title: "diablo", weight: 45}, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 object is inserted");
+        // });
+        // pizzasCollection.insertOne({title: "palermo", weight: 20}, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 object is inserted");
+        // });
+        // pizzasCollection.insertOne({title: "neapoly", weight: 36}, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 object is inserted");
+        // });
+        // pizzasCollection.insertOne({title: "ukr", weight: 80}, function(err, res) {
+        //     if (err) throw err;
+        //     console.log("1 object is inserted");
+        // });
 
         db.close();
     });
