dfe723b8385f6249651f463990702aeabe164ad3
Khrystyna Korinetska <kornecka@rambler.ru>
2019-07-28 20:08:45 +0300

    add icons to the filter(main file), make callback part of the main menu responsive

--- code/main/jq mainAction.js
+++ code/main/jq mainAction.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 $(".div-contact .dropdown-arrow .fa-sort-down").on("click", () => {
 	$(".callBack .div-contact .dropdown").slideToggle(400);
+	// $(".div-contact .dropdown-arrow").html("<i class='fas fa-sort-up'></i>");
 })
-
 
 });
